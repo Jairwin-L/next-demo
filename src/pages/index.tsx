c9cfa33a3fecb4731595d9fd844583c075d5e214
Jairwin <jairwin@jairwin.cn>
2023-06-09 18:16:17 +0800

    chore: commit repro code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
+import { useRef } from 'react';
 import Vimeo from '@u-wave/react-vimeo';
+import style from './index.module.scss';
 
-export default function Home() {
+export default function Main() {
+  const vimeoRef = useRef<any>(null);
+  const onRestart = () => {
+    console.log(`vimeoRef?.current----->：`, vimeoRef?.current);
+    if (vimeoRef?.current) {
+      // TODO:there is a problem here
+      vimeoRef.current.setCurrent(0);
+    }
+  };
   return (
-    <>
+    <div className={style.main}>
       <Vimeo
+        ref={vimeoRef}
+        className={style['vimeo-player']}
         video={'https://vimeo.com/831094569'}
+        muted
         loop
-        showPortrait={false}
-        showTitle={false}
-        showByline={false}
+        background
+        controls={false}
       />
-    </>
+      <button onClick={onRestart}>restart</button>
+    </div>
   );
 }
