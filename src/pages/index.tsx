0f8d457c01f5a0b85ed7ab8bfbada8baa64795fa
Jairwin <jairwin@jairwin.cn>
2023-06-09 17:27:23 +0800

    feat: access f2elint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Vimeo from "@u-wave/react-vimeo";
+import Vimeo from '@u-wave/react-vimeo';
 
 export default function Home() {
   return (
     <>
       <Vimeo
         video={'https://vimeo.com/831094569'}
-        loop={true}
+        loop
         showPortrait={false}
         showTitle={false}
         showByline={false}
